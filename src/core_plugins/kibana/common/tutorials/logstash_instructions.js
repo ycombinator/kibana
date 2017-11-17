db6b54f5b46c8006ea3efc9a3039b94b22c1df24
Shaunak Kashyap <ycombinator@gmail.com>
2017-11-17 06:56:12 -0800

    Refactor: extract common LS instructions (#8)

---
+++ src/core_plugins/kibana/common/tutorials/logstash_instructions.js
@@ -0,0 +1,14 @@
+export const LOGSTASH_INSTRUCTIONS = {
+  INSTALL: {
+    OSX: {
+      title: 'Download and install Logstash',
+      textPre: 'Skip this step if Logstash is already installed. First time using Logstash? See the ' +
+               '[Getting Started Guide]({config.elastic_docs.website_url}/guide/en/logstash/{config.elastic_docs.link_version}' +
+               '/getting-started-with-logstash.html).',
+      commands: [
+        'curl -L -O https://artifacts.elastic.co/downloads/logstash/logstash-{config.kibana.version}.tar.gz',
+        'tar xzvf logstash-{config.kibana.version}.tar.gz'
+      ]
+    }
+  }
+};
